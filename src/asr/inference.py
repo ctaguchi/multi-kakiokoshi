14f6d30316bbe5c620f17f6479dce1c2d1dd106e
田口智大 <ctaguchi@nd.edu>
2025-12-04 13:22:08 -0500

    add inference code

---
+++ src/asr/inference.py
@@ -0,0 +1,120 @@
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+import torch
+import pandas as pd
+from datasets import Dataset, Audio
+
+import argparse
+import os
+from typing import List, Dict, Any
+import glob
+
+
+TEST_DATA_DIR = "data/mdc_asr_shared_task_test_data"
+TEST_RESULTS_DIR = "data/test_results/"
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-l",
+        "--language",
+        type=str,
+        help="Language to test."
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        help="Model."
+    )
+    parser.add_argument(
+        "-t",
+        "--task_type",
+        type=str,
+        choices=["multilingual-general", "small-model", "unseen-langs"],
+        help="Task type."
+    )
+    parser.add_argument(
+        "-i",
+        "--id",
+        type=int,
+        help="Results ID."
+    )
+    return parser.parse_args()
+
+
+def load_testdata(test_data_dir: str,
+                  language: str) -> Dataset:
+    """Load the test data."""
+    audio_dir = os.path.join(test_data_dir, "audios")
+    pattern = os.path.join(audio_dir, f"spontaneous-speech-{language}-*.mp3")
+    test_audios = glob.glob(pattern)
+    test_dataset = Dataset.from_dict({"audio": test_audios})
+    test_dataset = test_dataset.cast_column("audio", Audio(sampling_rate=16000))
+    return test_dataset
+
+
+def batched_prediction(batch: Dict[str, Any],
+                       model: Wav2Vec2ForCTC,
+                       processor: Wav2Vec2Processor,
+                       device: str) -> Dict[str, Any]:
+    """Batched prediction for faster evaluation.
+    This should be run as `Dataset.map(batched_prediction, batched=True)`."""
+    # batch["audio"] is a list of dicts: {"array": np.ndarray, "sampling_rate": 16000}
+    arrays = [example["array"] for example in batch["audio"]]
+    inputs = processor(
+        arrays,
+        sampling_rate=16_000,
+        return_tensors="pt",
+        padding=True,
+    )
+    
+    with torch.no_grad():
+        logits = model(inputs.input_values.to(device)).logits
+    
+    pred_ids = torch.argmax(logits, dim=-1)
+    pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
+    batch["pred_str"] = pred_str
+    
+    return batch
+
+
+def main(args: argparse.Namespace):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    model = Wav2Vec2ForCTC.from_pretrained(
+        args.model,
+        target_lang=args.language
+    ).to(device)
+    
+    processor = Wav2Vec2Processor.from_pretrained(args.model)
+    processor.tokenizer.set_target_lang(args.language)
+    
+    # Load the test data
+    test_dataset = load_testdata(test_data_dir=TEST_DATA_DIR, language=args.language)
+    
+    # Run inference
+    test_dataset = test_dataset.map(batched_prediction,
+                                    batched=True,
+                                    fn_kwargs={
+                                        "model": model,
+                                        "processor": processor
+                                    })
+    preds: List[str] = test_dataset["pred_str"]
+    
+    # Load the test tsv sheet
+    tsv_dir = os.path.join(TEST_DATA_DIR, args.task_type) # e.g. data/mdc_asr_shared_task_test_data/multilingual-general
+    tsv_file = os.path.join(tsv_dir, f"{args.language}.tsv")
+    tsv = pd.read_csv(tsv_file, sep="\t")
+    tsv["sentence"] = preds
+    
+    # Save the results
+    output_dir = os.path.join(TEST_RESULTS_DIR, str(args.id), args.task_type) # e.g. data/test_results/1/multilingual-general
+    os.makedirs(output_dir, exist_ok=True)
+    results_file = os.path.join(output_dir, f"{args.language}.tsv")
+    tsv.to_csv(results_file, sep="\t")
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
