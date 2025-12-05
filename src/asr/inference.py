ccc51e77e8b42e8e73e0a684561a9ad4703500dc
田口智大 <ctaguchi@nd.edu>
2025-12-05 09:51:53 -0500

    debug inference

--- src/asr/inference.py
+++ src/asr/inference.py
@@ -1,4 +1,4 @@
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor, Wav2Vec2CTCTokenizer, Wav2Vec2FeatureExtractor
 import torch
 import pandas as pd
 from datasets import Dataset, Audio
@@ -160,8 +160,26 @@ def main(args: argparse.Namespace):
         processor = load_processor_with_temp_vocab(model_dir=model_dir_with_vocab,
                                                    temp_vocab_path=tmp_vocab_path)
     else:
-        processor = Wav2Vec2Processor.from_pretrained(model_dir_with_vocab)
-        processor.tokenizer.set_target_lang(args.language)
+        # processor = Wav2Vec2Processor.from_pretrained(model_dir_with_vocab)
+        # processor.tokenizer.set_target_lang(args.language)
+        feature_extractor = Wav2Vec2FeatureExtractor(
+            feature_size=1,
+            sampling_rate=16000,
+            padding_value=0.0,
+            do_normalize=True,
+            return_attention_mask=True
+        )
+        tokenizer = Wav2Vec2CTCTokenizer.from_pretrained(
+            model_dir_with_vocab,
+            unk_token="[UNK]",
+            pad_token="[PAD]",
+            word_delimiter_token="|",
+            target_lang=args.language
+        )
+        processor = Wav2Vec2Processor(
+            feature_extractor=feature_extractor,
+            tokenizer=tokenizer
+        )
     
     # make temporary flat vocab file
     # 3. Create a temp directory
