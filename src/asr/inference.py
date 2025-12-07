4a38a86b7e36a92d63d21bb91167488254a23955
田口智大 <ctaguchi@nd.edu>
2025-12-07 02:24:23 -0500

    adapt to xlsr models; fix typo

--- src/asr/inference.py
+++ src/asr/inference.py
@@ -157,7 +157,24 @@ def main(args: argparse.Namespace):
         vocab = json.load(f)
     
     if args.task_type == "small-model":
-        processor = Wav2Vec2Processor.from_pretrained(model_dir_with_vocab)
+        feature_extractor = Wav2Vec2FeatureExtractor(
+                feature_size=1,
+                sampling_rate=16000,
+                padding_value=0.0,
+                do_normalize=True,
+                return_attention_mask=True
+            )
+        tokenizer = Wav2Vec2CTCTokenizer.from_pretrained(
+                model_dir_with_vocab,
+                unk_token="[UNK]",
+                pad_token="[PAD]",
+                word_delimiter_token="|",
+            )
+        
+        processor = Wav2Vec2Processor(
+            feature_extractor=feature_extractor,
+            tokenizer=tokenizer
+        )
     else:
         # MMS-style
         flat_vocab = build_flat_vocab(vocab, lang=args.language)
